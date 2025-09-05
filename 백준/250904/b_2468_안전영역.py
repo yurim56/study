f8e75c0767fa9f857814e40a961dde24ab773f39
yurim56 <yoorim625@gmail.com>
2025-09-05 09:16:00 +0900

    Update b_2468_안전영역.py

--- 백준/250904/b_2468_안전영역.py
+++ 백준/250904/b_2468_안전영역.py
@@ -1,22 +1,22 @@
 dr = [1, -1, 0, 0]
 dc = [0, 0, 1, -1]
 
+
 def dfs(row, col, h):
-    
     visited[row][col] = 1
-    
+
     for dir in range(4):
         nr = row + dr[dir]
         nc = col + dc[dir]
-        
+
         if 0 <= nr < N and 0 <= nc < N and graph[nr][nc] > h and visited[nr][nc] == 0:
-            dfs(nr, nc, h)     
+            dfs(nr, nc, h)
+
 
 N = int(input())
 
 graph = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0]*N for _ in range(N)]
-safe = 0
+answer = 0
 
 # max_h = max(map(max, graph))
 best = -9999999999999999999
@@ -24,13 +24,15 @@ for i in graph:
     for v in i:
         if v > best:
             best = v
-            
-# for 
-#     for r in range(N):
-#         for c in range(N):
-#             if graph[r][c] <= 4:
-#                 graph[r][c] = 0
-#             elif visited[r][c] != 1:
-#                 graph[r][c] = 1
-#                 dfs(r, c)
-#                 safe += 1
+
+for h in range(best+1):
+    visited = [[0] * N for _ in range(N)]
+    safe = 0
+    for r in range(N):
+        for c in range(N):
+            if graph[r][c] > h and visited[r][c] == 0:
+                dfs(r, c, h)
+                safe += 1
+    answer = max(answer, safe)
+
+print(answer)
