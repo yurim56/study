19e564fc2c61945f3f2ec24cf29c834331b20608
yurim56 <yoorim625@gmail.com>
2025-09-09 09:30:32 +0900

    .

---
+++ swea/9월/250909/swea_20551_증가하는사탕수열.py
@@ -0,0 +1,31 @@
+# 1. B를 C보다 작게
+# 2. A를 B보다 작게
+# ---------------------설계---------------------
+# 완전 탐색부터 파악하기 -> 그 다음 규칙 세우기
+# 완탐 -> B를 C보다 작을 때까지 하나씩 먹자 -> A를 B보다 작을 때까지 하나씩 먹자
+#      -> 너무 비효율적임
+# 규칙 -> B = C - 1 / A = B - 1
+
+T = int(input())
+
+for tc in range(1, T+1):
+    A, B, C = map(int, input().split())
+    
+    # 불가능한 케이스를 먼저 지우자
+    if A < 1 or B < 2 or C < 3:
+        print(f'#{tc} -1')
+        continue
+    
+    eat_count = 0
+    
+    # B상자 = C상자 -1
+    if B >= C:
+        eat_count += B - (C-1)
+        B = C - 1
+            
+    # A상자 = B상자 -1
+    if A >= B:
+        eat_count += A - (B-1)
+        A = B - 1
+        
+    print(f'#{tc} {eat_count}')
